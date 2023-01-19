532d86ee5ff2f4868317daac9d4cad5300d8aa03
Nutthinun-526 <Nutthinun75587@gmail.com>
2023-01-19 10:32:15 +0700

    Week10

---
+++ Week10/src/WeeklyWeatherData.java
@@ -0,0 +1,43 @@
+
+public class WeeklyWeatherData {
+
+	//attributes
+	static int NUMDAYS = 7;
+	private double[] rainFall = new double[NUMDAYS];
+	private double[] avgPressure = new double[NUMDAYS];
+	private double avgRain;
+	private double maxRain;
+	//methods
+	//setter methods
+	
+	public void setData(double[] rain,double[] pressure) {
+		rainFall = rain;
+		avgPressure = pressure;
+	}//end of setData()
+	
+	public void calculateStats() {
+		maxRain = 0.0;
+		double rainSum = 0.0;
+		
+		for(int i = 0; i < rainFall.length; i++) {
+			if(rainFall[i] >= maxRain) {
+				maxRain = rainFall[i];
+				
+			}//end of if
+			rainSum += rainFall[i];
+		}//end of for
+		avgRain = rainSum / NUMDAYS;
+	}//end of calculateStatus()
+	
+	//getter methods
+	
+	public double getAvgRain() {
+		return avgRain;
+	}
+	
+	public double getMaxRain() {
+		return maxRain;
+	}
+	
+
+}
